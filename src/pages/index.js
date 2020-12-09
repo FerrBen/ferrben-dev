2b68e682a92686b96af350daedbc6b61fb295f67
FerrBen <qweetr86@gmail.com>
2020-12-09 08:28:38 +0100

    adding tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styles from 'styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+    <h1 className=' text-4xl'>test</h1>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
